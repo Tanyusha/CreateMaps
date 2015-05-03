45b780c05c450a27f378f97b8e3c6df427a2969d
Tanyusha <tatiana_zyablikova@mail.ru>
2015-05-03 14:31:15 +0500

    create step 4 and 5

--- _project_/urls.py
+++ _project_/urls.py
@@ -37,11 +37,11 @@ def create_step_2(request):
     file = request.session.get('step-1-filepath')
 
     name, ext = os.path.splitext(filename)
-    Db = DATABESE_TYPES.get(ext)
-    if not Db:
+    DBase = DATABESE_TYPES.get(ext)
+    if not DBase:
         return render(request, 'error-db-not-supported.html')
 
-    fields = Db.required_info_for_init()
+    fields = DBase.required_info_for_init()
     Form = make_form(fields)
     POST = request.POST.copy()
     POST.update({'file': file})
@@ -49,7 +49,8 @@ def create_step_2(request):
     if request.method == "POST" and form.is_valid():
         try:
             data = form.cleaned_data
-            Db(**data)
+            db = DBase(**data)
+            db.close()
             request.session['step-2-database-init-data'] = data
             request.session['step-2-database'] = ext
         except Exception as e:
@@ -61,6 +62,7 @@ def create_step_2(request):
 
 
 def create_step_3(request):
+    form = forms.BaseForm
     data = request.session.get('step-2-database-init-data')
     ext = request.session.get('step-2-database')
     DBase = DATABESE_TYPES.get(ext)
@@ -69,6 +71,7 @@ def create_step_3(request):
 
     db = DBase(**data)
     tables = db.tables()
+    db.close()
 
     # for table in tables:
     #     print(tables[table].name)
@@ -76,8 +79,40 @@ def create_step_3(request):
     #         print(column.name)
         #     column_list.append(column.name)
         # tables_list['tables[table].name']=column_list
+    if request.method == "POST":
+        request.session["step-3-selected-table"] = request.POST['table']
+        return redirect('step4')
     return render(request, 'create-3.html', {'tables': tables})
 
+
+def create_step_4(request):
+    ext = request.session.get('step-2-database')
+    data = request.session.get('step-2-database-init-data')
+    selected_table = request.session["step-3-selected-table"]
+
+    DBase = DATABESE_TYPES.get(ext)
+    db = DBase(**data)
+    tables = db.tables()
+    db.close()
+    column_list = []
+    for table in tables:
+        print(table)
+        if table == selected_table:
+            for column in tables[table].columns:
+                column_list.append(column.name)
+            break
+    if request.method == "POST":
+        request.session["step-4-coord-X"] = request.POST['coord-X']
+        request.session["step-4-coord-Y"] = request.POST['coord-Y']
+        return redirect('step5')
+    return render(request, 'create-4.html', {'table': selected_table, 'columns': column_list})
+
+def create_step_5(request):
+    ext = request.session.get('step-2-database')
+    coord_x = request.session.get("step-4-coord-X")
+    coord_y = request.session.get("step-4-coord-Y")
+
+    return render(request, 'create-5.html', {'coord_X': coord_x, 'coord_Y': coord_y})
 
 urlpatterns = [
     # Examples:
@@ -89,5 +124,7 @@ urlpatterns = [
     url(r'^create-1$', create_step_1, name='step1'),
     url(r'^create-2$', create_step_2, name='step2'),
     url(r'^create-3$', create_step_3, name='step3'),
+    url(r'^create-4$', create_step_4, name='step4'),
+    url(r'^create-5$', create_step_5, name='step5'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- dbase/admin.py
+++ dbase/admin.py
@@ -1,3 +1,6 @@
+import dbase
 from django.contrib import admin
 
+
 # Register your models here.
+admin.site.register(dbase)
