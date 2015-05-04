fa0d9f83b18eac335b1af58e314d724bf7cbdbe0
Tanyusha <tatiana_zyablikova@mail.ru>
2015-05-04 16:28:16 +0500

    improve the styles

--- _project_/static/script.js
+++ _project_/static/script.js
@@ -7,25 +7,19 @@ $('.non-checked-btn-for-table').click(function (event) {
     event.preventDefault(); // cancel default behavior
     var table_name = $(this).attr('table-name');
     var p_id = "#p-for-"+ table_name;
-    var span_id = "#span-for-"+ table_name;
-    $(this).removeClass('non-checked-btn-for-table');
-    $(this).addClass('checked-btn-for-table');
+    var button_id = "#button-close-for-"+ table_name;
+    $(this).css('display', 'none');
+    $(button_id).css('display', 'block');
     $(p_id).css('display', 'block');
     $(p_id).css('margin', '10px 0 0 0');
-    $(span_id).removeClass('glyphicon-plus');
-    $(span_id).addClass('glyphicon-minus');
 });
 
 $('.checked-btn-for-table').click(function (event) {
-    alert("lalal");
     event.preventDefault(); // cancel default behavior
     var table_name = $(this).attr('table-name');
     var p_id = "#p-for-"+ table_name;
-    var span_id = "#span-for-"+ table_name;
-    $(this).removeClass('checked-btn-for-table');
-    $(this).addClass('non-checked-btn-for-table');
-    alert("lalal");
+    var button_id = "#button-open-for-"+ table_name;
+    $(button_id).css('display', 'block');
+    $(this).css('display', 'none');
     $(p_id).css('display', 'none');
-    $(span_id).removeClass('glyphicon-minus');
-    $(span_id).addClass('glyphicon-plus');
 });
